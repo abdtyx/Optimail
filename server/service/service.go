14a93dd3a5440ceef97ee1ff568f8ab0123ffd13
abdtyx <1507844577@qq.com>
2024-12-06 13:39:59 -0600

    server api done

--- server/service/service.go
+++ server/service/service.go
@@ -1,8 +1,15 @@
 package service
 
 import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
 	"log"
 	"net/http"
+	"time"
 
 	pb "github.com/abdtyx/Optimail/micro-user/dto"
 	"google.golang.org/grpc"
@@ -83,40 +90,61 @@ func (s *Service) RootHandler(c *gin.Context) {
 	Main Functionality
 */
 
+type summarizeReq struct {
+	Email   string `json:"email"`
+	Content string `json:"content"`
+}
+
 func (s *Service) Summarize(c *gin.Context) {
 	// Get content from mail server
-	// var req dto.SummaryRequest
-	// if err := c.ShouldBindJSON(&req); err != nil {
-	// 	log.Println("**ERROR**: /summarize", err)
-	// 	c.JSON(http.StatusInternalServerError, "Server error")
-	// 	return
-	// }
-	// log.Println(req)
+	var req summarizeReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("**ERROR**: /summarize", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "email and content required")
+		return
+	}
+	log.Println(req)
 
-	// auth
+	// auth (skip now)
 
 	// query db
-	// setting, err := userclient.GetSettingsBySecret(req.Secret)
-	// if err != nil {
-	// 	c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
-	// 		"error": gin.H{
-	// 			"code":    "BadServiceGetSettingsBySecretRequest",
-	// 			"message": "Bad credentials",
-	// 		},
-	// 	})
-	// }
+	var idReq = &pb.UsersGetIdByEmailRequest{
+		Email: req.Email,
+	}
+	idResp, err := s.userclient.UsersGetIdByEmail(context.Background(), idReq)
+	if err != nil {
+		log.Println("**ERROR**: /summarize", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "id not found")
+		return
+	}
+	var settingsReq = &pb.UsersGetSettingsRequest{
+		Id: idResp.Id,
+	}
+	settingsResp, err := s.userclient.UsersGetSettings(context.Background(), settingsReq)
+	if err != nil {
+		log.Println("**ERROR**: /summarize", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "settings not found")
+		return
+	}
 
 	// call chatgpt
-	content := setting.Prompt + req.Content
+	content := settingsResp.Settings.SummaryPrompt + "\nEmail:\n" + req.Content
 	gptresp, err := s.gpt.Chat(content)
 	if err != nil {
 		log.Println("**ERROR**: /summarize", err)
-		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-			"message": "gpt failed",
-		})
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "gpt failed")
 	}
 
 	// handle gpt's resp
+	var createSummaryRequest = &pb.UsersCreateSummaryRequest{
+		Id:      idResp.Id,
+		Summary: gptresp,
+	}
+	_, err = s.userclient.UsersCreateSummary(context.Background(), createSummaryRequest)
+	if err != nil {
+		log.Println("**ERROR**: /summarize", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "update db failed")
+	}
 
 	// ack
 	c.JSON(http.StatusOK, gin.H{
@@ -124,29 +152,66 @@ func (s *Service) Summarize(c *gin.Context) {
 	})
 }
 
+type emphasizeReq struct {
+	Email   string `json:"email"`
+	Content string `json:"content"`
+}
+
 func (s *Service) Emphasize(c *gin.Context) {
-	var req dto.EmphasizeRequest
+	// Get content from mail server
+	var req emphasizeReq
 	if err := c.ShouldBindJSON(&req); err != nil {
 		log.Println("**ERROR**: /emphasize", err)
-		c.JSON(http.StatusInternalServerError, "Server error")
+		c.AbortWithStatusJSON(http.StatusBadRequest, "email and content required")
 		return
 	}
 	log.Println(req)
 
-	// auth
+	// auth (skip now)
 
 	// query db
+	var idReq = &pb.UsersGetIdByEmailRequest{
+		Email: req.Email,
+	}
+	idResp, err := s.userclient.UsersGetIdByEmail(context.Background(), idReq)
+	if err != nil {
+		log.Println("**ERROR**: /emphasize", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "id not found")
+		return
+	}
+	var settingsReq = &pb.UsersGetSettingsRequest{
+		Id: idResp.Id,
+	}
+	settingsResp, err := s.userclient.UsersGetSettings(context.Background(), settingsReq)
+	if err != nil {
+		log.Println("**ERROR**: /emphasize", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "settings not found")
+		return
+	}
 
 	// call chatgpt
+	content := settingsResp.Settings.EmphasisPrompt + "\nEmail:\n" + req.Content
+	gptresp, err := s.gpt.Chat(content)
+	if err != nil {
+		log.Println("**ERROR**: /emphasize", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "gpt failed")
+	}
 
 	// handle gpt's resp
-
-	// construct resp
-
-	resp := dto.EmphasizeResponse{
-		Emphasis: "TODO:",
+	var createEmphasisRequest = &pb.UsersCreateEmphasisRequest{
+		Id:      idResp.Id,
+		Summary: gptresp,
 	}
-	c.JSON(http.StatusOK, resp)
+	_, err = s.userclient.UsersCreateEmphasis(context.Background(), createEmphasisRequest)
+	if err != nil {
+		log.Println("**ERROR**: /summarize", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, "update db failed")
+	}
+
+	// ack
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Emphasis done",
+	})
 }
 
 /*
@@ -154,29 +219,204 @@ func (s *Service) Emphasize(c *gin.Context) {
 */
 
 func (s *Service) CreateUser(c *gin.Context) {
-
+	var req pb.UsersCreateUserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("**ERROR**: CreateUser:", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "Username and password required")
+		return
+	}
+	resp, err := s.userclient.UsersCreateUser(context.Background(), &req)
+	if err != nil {
+		log.Println("**ERROR**: UsersCreateUser:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"id":      resp.Id,
+		"pk":      resp.Pk,
+		"message": resp.Message,
+	})
 }
 
 func (s *Service) Login(c *gin.Context) {
-
+	var req pb.UsersLoginRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("**ERROR**: Login:", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "Username and password required")
+		return
+	}
+	resp, err := s.userclient.UsersLogin(context.Background(), &req)
+	if err != nil {
+		log.Println("**ERROR**: UsersLogin:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	// generate jwt
+	jwtstr, err := genJwt(resp.Id, time.Now().Add(2*time.Hour), []byte(s.cfg.JWTKey))
+	if err != nil {
+		log.Println("**ERROR**: Login:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.Header("Authentication", jwtstr)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login successful",
+	})
 }
 
 func (s *Service) Logout(c *gin.Context) {
-
+	jwtstr := c.GetHeader("Authentication")
+	id, err := verifyJwt(jwtstr, []byte(s.cfg.JWTKey))
+	if err != nil {
+		log.Println("**ERROR**: Logout:", err)
+		c.AbortWithStatusJSON(http.StatusForbidden, err)
+		return
+	}
+	var req = &pb.UsersLogoutRequest{
+		Id: id,
+	}
+	_, err = s.userclient.UsersLogout(context.Background(), req)
+	if err != nil {
+		log.Println("**ERROR**: UsersLogout:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Logout successful",
+	})
 }
 
 func (s *Service) ChangePwd(c *gin.Context) {
-
+	var req pb.UsersChangePwdRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		log.Println("**ERROR**: ChangePwd:", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "username ,old_password, and new_password required")
+		return
+	}
+	_, err := s.userclient.UsersChangePwd(context.Background(), &req)
+	if err != nil {
+		log.Println("**ERROR**: UsersChangePwd:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Password changed",
+	})
 }
 
 func (s *Service) GetSettings(c *gin.Context) {
-
+	jwtstr := c.GetHeader("Authentication")
+	id, err := verifyJwt(jwtstr, []byte(s.cfg.JWTKey))
+	if err != nil {
+		log.Println("**ERROR**: GetSettings:", err)
+		c.AbortWithStatusJSON(http.StatusForbidden, err)
+		return
+	}
+	var req = &pb.UsersGetSettingsRequest{
+		Id: id,
+	}
+	resp, err := s.userclient.UsersGetSettings(context.Background(), req)
+	if err != nil {
+		log.Println("**ERROR**: UsersGetSettings:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"settings": resp,
+		"message":  "OK",
+	})
 }
 
 func (s *Service) UpdateSettings(c *gin.Context) {
+	jwtstr := c.GetHeader("Authentication")
+	id, err := verifyJwt(jwtstr, []byte(s.cfg.JWTKey))
+	if err != nil {
+		log.Println("**ERROR**: Logout:", err)
+		c.AbortWithStatusJSON(http.StatusForbidden, err)
+		return
+	}
 
+	var req = &pb.UsersUpdateSettingsRequest{
+		Id: id,
+	}
+	if err := c.ShouldBindJSON(req.Settings); err != nil {
+		log.Println("**ERROR**: UpdateSettings:", err)
+		c.AbortWithStatusJSON(http.StatusBadRequest, "settings required")
+		return
+	}
+
+	_, err = s.userclient.UsersUpdateSettings(context.Background(), &req)
+	if err != nil {
+		log.Println("**ERROR**: UsersUpdateSettings:", err)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, err)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Settings updated",
+	})
 }
 
-func (s *Service) ResetSecret(c *gin.Context) {
+/*
+	JWT
+*/
 
+func hmacEval(key []byte, msg []byte) ([]byte, error) {
+	mac := hmac.New(sha512.New, key)
+	mac.Write(msg)
+	res := mac.Sum(nil)
+
+	return res, nil
+}
+
+type jwt struct {
+	ID     string    // uuid
+	Expire time.Time // valid through
+}
+
+/*
+	Simplified JWT. Only provide Integrity, no Authenticity.
+*/
+
+func genJwt(id string, expire time.Time, key []byte) (jwtstr string, err error) {
+	jwtstr = ""
+	j := jwt{
+		ID:     id,
+		Expire: expire,
+	}
+	b, err := json.Marshal(j)
+	if err != nil {
+		return
+	}
+	// generate MAC tag
+	mac, err := hmacEval(key, b)
+	if err != nil {
+		return
+	}
+	return hex.EncodeToString(append(b, mac...)), nil
+}
+
+func verifyJwt(jwtstr string, key []byte) (id string, err error) {
+	jwtbyte := []byte(jwtstr)
+	// check length
+	l := len(jwtbyte)
+	if l <= 64 {
+		return "", errors.New("invalid jwt")
+	}
+	// retrieve jwt byte
+	b := jwtbyte[:l-64]
+	// generate MAC tag
+	mac, err := hmacEval(key, b)
+	if err != nil {
+		return "", err
+	}
+	// compare MAC tags
+	if !hmac.Equal(mac, jwtbyte[l-64:]) {
+		return "", errors.New("invalid jwt")
+	}
+	// retrieve ID
+	var j jwt
+	if err := json.Unmarshal(b, &j); err != nil {
+		return "", err
+	}
+	return j.ID, nil
 }
